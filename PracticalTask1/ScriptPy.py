9f9b89060d89800c3abf7af270707147baa315a3
OsipovMax <maxim.osipov96@gmail.com>
2018-10-09 13:47:05 +0300

    changes_v2

--- PracticalTask1/ScriptPy.py
+++ PracticalTask1/ScriptPy.py
@@ -2,12 +2,13 @@
 import shlex, subprocess
 
 log = open('rep.txt','w')
-res = 0.0
+
 for i in range(6):
+    res = 0.0
     for j in range(10):
         c = subprocess.check_output(["./main","_rA","_rB","_rC",str(i)])
         l = c.decode("utf-8")
-    res = res + int(l)
+	res = res + int(l)
     res = res / 10
     log.write(str(i))
     log.write(" ")
