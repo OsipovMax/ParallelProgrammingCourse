907cd160d0d7cd0be6059e05727bd88a88cfc0d6
OsipovMax <maxim.osipov96@gmail.com>
2018-11-20 17:50:57 +0300

    final changes

--- PracticalTask4/main.cpp
+++ PracticalTask4/main.cpp
@@ -11,23 +11,21 @@
 
 using namespace std;
 template <typename T>
-double* mult(Matrix<T> &m, double* v ) {
+double* mult(Matrix<T> &m, double* v){
 	double *result;
 	result = new double[m.getRows()];
-	for (int i = 0; i<m.getRows(); ++i){
-		for (int j = 0; j<m.getColumns(); ++j){
+	memset((void*)result, 0, m.getRows());
+	for (int i = 0; i < m.getRows(); ++i){
+		for (int j = 0; j < m.getColumns(); ++j){
 			result[i] += m.mtr[i][j] * v[j];
 		}
 	}
-
-	
 	return result;
 	
 }
 
-
 template <typename T>
-Matrix<T> binReader(const std::string &strr) {
+Matrix<T> binReader(const std::string &strr){
 
 	int modeVal, n, m;
 	double val;
@@ -43,9 +41,9 @@ Matrix<T> binReader(const std::string &strr) {
 	tempObj.setColumns(m);
 
 	tempObj.mtr.resize(n);
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i){
 		tempObj.mtr[i].resize(m);
-		for (int j = 0; j < m; j++) {
+		for (int j = 0; j < m; j++){
 			f1.read((char*)&val, sizeof(double));
 			tempObj.mtr[i][j] = val;
 		}
@@ -56,14 +54,14 @@ Matrix<T> binReader(const std::string &strr) {
 
 
 template <typename T>
-double* toBuf ( Matrix<T> &m , int trig ) {
+double* toBuf (Matrix<T> &m, int trig) {
 	
 	int inner_counter = 0;
 	int numproces;
 	double* buf = new double[m.getColumns() * m.getRows()];
-	if (trig == 0) {
+	if (trig == 0){
 		for (int i = 0; i < m.getRows(); ++i){
-			for (int j =0; j< m.getColumns(); ++j){
+			for (int j =0; j < m.getColumns(); ++j){
 				buf[inner_counter] = m.mtr[i][j];
 				inner_counter++;
 			}
@@ -71,11 +69,11 @@ double* toBuf ( Matrix<T> &m , int trig ) {
 	}
 	else {
 		MPI_Comm_size(MPI_COMM_WORLD, &numproces);
-		int temp_size = (int)(m.getColumns()/numproces);
+		int temp_size = (int)(m.getColumns() / numproces);
 		for (int k = 0; k < numproces; ++k){
 			for (int i = 0; i < m.getRows();++i){
 				for (int j =0; j < temp_size; ++j){
-					buf[inner_counter] = m.mtr[i][j+k*temp_size];
+					buf[inner_counter] = m.mtr[i][j + k * temp_size];
 					inner_counter++;
 				}
 			}
@@ -85,7 +83,7 @@ double* toBuf ( Matrix<T> &m , int trig ) {
 }
 
 template <typename T>
-Matrix<T> toMatr(int r_size, int c_size, double *buf ) {
+Matrix<T> toMatr(int r_size, int c_size, double *buf) {
 	int inner_counter = 0;
 
 	Matrix<T> tempObj;
@@ -109,10 +107,10 @@ int main(int argc, char *argv[])
 	int left_limit, right_limit;
 	int count_rows, count_columns;
 	int n_size, m_size;
-	int numproces,rank;
+	int numproces, rank;
 	double time_total = 0.0, time_max = 0.0;
 	double time_begin = 0.0, time_end = 0.0;
-	string file_A, file_B,file_C;
+	string file_A, file_B, file_C;
 
 
 	file_A = argv[1];
@@ -122,7 +120,7 @@ int main(int argc, char *argv[])
 	Matrix<double> matr;
 	Matrix<double> vect;
 	
-	if ((retval = MPI_Init(&argc, &argv)) != MPI_SUCCESS) {
+	if ((retval = MPI_Init(&argc, &argv)) != MPI_SUCCESS){
 		cout << " MPI_Init error " << endl;
 		MPI_Abort(MPI_COMM_WORLD, retval);
 	}
@@ -137,7 +135,7 @@ int main(int argc, char *argv[])
 	double *rb;
 	double *mull_res_vect;
 	
-	if (rank == 0 ) {
+	if (rank == 0){
 		
 		matr = binReader<double>(file_A);
 		vect = binReader<double>(file_B);
@@ -146,17 +144,17 @@ int main(int argc, char *argv[])
 		m_size = matr.getColumns();
 
 		
-		if (matr.getRows() > matr.getColumns()){
+		if (matr.getRows() >= matr.getColumns()){
 			triger = 0;
-			left_limit = rank*n_size/numproces;
-			right_limit = (rank+1)*n_size/numproces - 1;
-			count_rows = right_limit - left_limit +1 ;
+			left_limit = rank * n_size / numproces;
+			right_limit = (rank + 1) * n_size / numproces - 1;
+			count_rows = right_limit - left_limit + 1 ;
 		}
 		else {
 			triger = 1;
-			left_limit = rank*m_size/numproces;
-			right_limit = (rank+1)*m_size/numproces - 1;
-			count_columns = right_limit - left_limit +1 ;
+			left_limit = rank * m_size / numproces;
+			right_limit = (rank + 1) * m_size / numproces - 1;
+			count_columns = right_limit - left_limit + 1 ;
 		}
 		
 	}
@@ -168,42 +166,41 @@ int main(int argc, char *argv[])
 		MPI_Bcast(&n_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(&m_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(&count_rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-		vector_buf = new double[m_size];
 			
 		if ( rank == 0 ){
 			matr_buf = toBuf<double>(matr,0);
 			vector_buf = toBuf<double>(vect,0);
+		}
+		else {
+			vector_buf = new double[m_size];
 		}
 
 		MPI_Bcast(vector_buf, m_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 		MPI_Barrier(MPI_COMM_WORLD);
 			
-		proc_matr_buf = new double [m_size*count_rows];
+		proc_matr_buf = new double [m_size * count_rows];
 
-		MPI_Scatter(matr_buf, m_size*count_rows, MPI_DOUBLE, proc_matr_buf, m_size*count_rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		MPI_Scatter(matr_buf, m_size * count_rows, MPI_DOUBLE, proc_matr_buf, m_size * count_rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-		Matrix<double> mull_matr = toMatr<double>(count_rows,m_size,proc_matr_buf); 
-
-		mull_res_vect = new double[count_rows]; 
+		Matrix<double> mull_matr = toMatr<double>(count_rows, m_size, proc_matr_buf); 
 
 		time_begin = MPI_Wtime();
 
-		mull_res_vect = mult(mull_matr,vector_buf);  
+		mull_res_vect = mult(mull_matr, vector_buf);  
 		
 		time_end = MPI_Wtime() - time_begin;
 
 		
-		if( rank!=0){
+		if( rank != 0){
 			MPI_Send(&time_end, 1, MPI_DOUBLE, 0, rank, MPI_COMM_WORLD);
 		}
 			
-		delete proc_matr_buf;
-		delete vector_buf;
+		delete[] proc_matr_buf;
+		delete[] vector_buf;
 
 		if (rank == 0) {
-			delete matr_buf;
+			delete[] matr_buf;
 		}
 
 
@@ -214,17 +211,17 @@ int main(int argc, char *argv[])
 			rb = new double [n_size];
 			if( time_end > time_max)
 				time_max = time_end;
-			for (int i = 0; i< numproces -1; ++i){
+			for (int i = 0; i < numproces -1; ++i){
 				MPI_Recv(&time_end, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 				if( time_end > time_max)
 					time_max = time_end;
-				time_total+=time_end;
+				time_total += time_end;
 			}
 
 		}
 		MPI_Gather(mull_res_vect, count_rows , MPI_DOUBLE, rb, count_rows, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 			
-		delete mull_res_vect;
+		delete[] mull_res_vect;
 
 	}
 		
@@ -233,30 +230,38 @@ int main(int argc, char *argv[])
 		MPI_Bcast(&m_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(&count_columns, 1, MPI_INT, 0, MPI_COMM_WORLD);
 			
-		vector_buf= new double[m_size];
 		if ( rank == 0 ){
-			matr_buf = toBuf<double>(matr,1);
-			vector_buf = toBuf<double>(vect,0);
+			matr_buf = toBuf<double>(matr, 1);
+			vector_buf = toBuf<double>(vect, 0);
+		}
+		else {
+			vector_buf= new double[m_size];
 		}
 			
-		proc_matr_buf = new double [n_size*count_columns];
+		proc_matr_buf = new double [n_size * count_columns];
 		proc_vect_buf = new double [count_columns];
 
-		MPI_Scatter(matr_buf, n_size*count_columns, MPI_DOUBLE, proc_matr_buf, n_size*count_columns, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+		MPI_Scatter(matr_buf, n_size * count_columns, MPI_DOUBLE, proc_matr_buf, n_size * count_columns, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 		MPI_Scatter(vector_buf, count_columns, MPI_DOUBLE, proc_vect_buf, count_columns, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 			
-		Matrix<double> mull_matr = toMatr<double>(n_size,count_columns, proc_matr_buf);
+		Matrix<double> mull_matr = toMatr<double>(n_size, count_columns, proc_matr_buf);
 
 		time_begin = MPI_Wtime();
-		double *mull_res_vect = mult(mull_matr,proc_vect_buf);
+		mull_res_vect = mult(mull_matr, proc_vect_buf);
 		time_end = MPI_Wtime() - time_begin;
 			
-		if( rank!=0){
+		if(rank != 0){
 		MPI_Send(&time_end, 1, MPI_DOUBLE, 0, rank, MPI_COMM_WORLD);
 		}
 			
-		delete proc_matr_buf;
-		delete proc_vect_buf;
+		delete[] proc_matr_buf;
+		delete[] proc_vect_buf;
+		delete[] vector_buf;
+
+		if (rank == 0) {
+			delete[] matr_buf;
+		}
+
 
 		MPI_Status status;
 		if ( rank == 0 ){
@@ -264,7 +269,7 @@ int main(int argc, char *argv[])
 			if (time_end > time_max)
 				time_max = time_end;
 			rb = new double [n_size];
-			for (int i = 0; i< numproces -1; ++i){
+			for (int i = 0; i < numproces -1; ++i){
 				MPI_Recv(&time_end, 1, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 				if( time_end > time_max)
 					time_max = time_end;
@@ -273,10 +278,11 @@ int main(int argc, char *argv[])
 	
 		}
 		MPI_Reduce(mull_res_vect, rb, n_size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+		delete[] mull_res_vect;
 	}
 	
-	if (rank ==0){
-		int rcol=1;
+	if (rank == 0){
+		int rcol = 1;
 		char tp = 'd';
 		if (numproces == 1) {
 			ofstream fout1p ("rproc1", ios::out);
@@ -289,8 +295,8 @@ int main(int argc, char *argv[])
 			ifstream fin ("rproc1", ios::in);
 			fin >> tm;
 			fin.close();
-			speedup = (double)tm/time_max;
-			efficiency = speedup/numproces;
+			speedup = (double)tm / time_max;
+			efficiency = speedup / numproces;
 			ofstream fout("result", ios::app);
 			fout << numproces << " " << time_total << " " << time_max << " " << speedup << " " << efficiency <<endl; 
 			fout.close();
@@ -306,5 +312,6 @@ int main(int argc, char *argv[])
 		
 	}
 	MPI_Finalize();
+	delete[] rb;
 	return 0;
 }
